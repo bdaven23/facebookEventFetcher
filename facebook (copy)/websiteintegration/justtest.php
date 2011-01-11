31ec61d2ef22f073aa3c96f766bc868f7887e7a1
Bishnu Prasad Pandey <pandeybishnupd@gmail.com>
2011-01-11 00:20:39 -0700

    Delted

--- facebook (copy)/websiteintegration/justtest.php
+++
@@ -1,6 +0,0 @@
-<a href="https://graph.facebook.com/oauth/authorize?
-    client_id=150265945018990&
-    redirect_uri=http://www.dhaubaji.info/testpassed.php&
-    scope=user_photos,user_videos,publish_stream,user_events&
-    type=user_agent&
-    display=popup">Click me</a>
